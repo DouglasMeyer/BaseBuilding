d7fcc51953bf6601a2f04dc9c7cce7cb6275d66d
Douglas Meyer <Douglas.Meyer@mail.com>
2015-12-16 14:14:26 -0600

    Cleanup update.js by moving tile generation into change_state.js

--- src/update.js
+++ src/update.js
@@ -7,32 +7,9 @@ var changeState = require('./update/change_state'),
 module.exports = function update(timeDelta, input, gameState){
   return [
     changeState.bind(null, input),
+    loadTextures,
     resizeWindow.bind(null, input),
     moveScreen.bind(null, input),
     toggleTile.bind(null, input),
-    function generateTiles(gameState){
-      if (gameState.world.tiles) return;
-
-      var newGameState = copyWith(gameState, {
-        world: {
-          tiles: [],
-        }
-      });
-      for (var y=0;y<100;y++){
-        var row = [];
-        newGameState.world.tiles.push(row);
-        for (var x=0;x<100;x++){
-          row.push({
-            type: Math.random() > 0.5 ? 'empty' : 'floor',
-            looseObject: null,
-            installedObject: null,
-            x: x,
-            y: y
-          });
-        }
-      }
-      return newGameState;
-    },
-    loadTextures,
   ].reduce(function updateReduceChanges(gameState, f){ return f(gameState) || gameState; }, gameState);
 };
--- src/update/change_state.js
+++ src/update/change_state.js
@@ -1,14 +1,34 @@
 /*
  * undefined --start--> 'playing'
  */
+
+function start(gameState){
+  var newGameState = copyWith(gameState, {
+    state: 'playing',
+    world: { tiles: [] }
+  });
+
+  for (var y=0;y<100;y++){
+    var row = [];
+    newGameState.world.tiles.push(row);
+    for (var x=0;x<100;x++){
+      row.push({
+        type: Math.random() > 0.5 ? 'empty' : 'floor',
+        looseObject: null,
+        installedObject: null,
+        x: x,
+        y: y
+      });
+    }
+  }
+  return newGameState;
+}
+
 module.exports = function changeState(input, gameState){
   switch(gameState.state){
 
     case undefined:
-      // start
-      return copyWith(gameState, {
-        state: 'playing'
-      });
+      return start();
 
     case 'playing':
       break;
